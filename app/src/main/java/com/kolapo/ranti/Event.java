d4a7e717d2e3d27d74aac74dea091dfdd4cc9774
Kolapo Oni <onikolapo@gmail.com>
2015-08-01 05:52:44 +0100

    setup event model

---
+++ app/src/main/java/com/kolapo/ranti/Event.java
@@ -0,0 +1,16 @@
+package com.kolapo.ranti;
+
+import android.media.Image;
+
+/**
+ * Created by Kolapo on 8/1/2015.
+ */
+public class Event {
+    public String name;
+    public String date;
+
+    public Event(String name, String date){
+        this.name = name;
+        this.date = date;
+    }
+}
---
+++ app/src/main/java/com/kolapo/ranti/EventRecycleViewAdapter.java
@@ -0,0 +1,7 @@
+package com.kolapo.ranti;
+
+/**
+ * Created by Kolapo on 8/1/2015.
+ */
+public class EventRecycleViewAdapter {
+}
