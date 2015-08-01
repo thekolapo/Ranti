bce33593c393bc7c114215a1bf6a4db5e0ab64e4
Kolapo Oni <onikolapo@gmail.com>
2015-08-01 07:54:54 +0100

    setup recycler adapter

--- app/src/main/java/com/kolapo/ranti/EventRecycleViewAdapter.java
+++ app/src/main/java/com/kolapo/ranti/EventRecycleViewAdapter.java
@@ -1,7 +1,62 @@
 package com.kolapo.ranti;
 
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by Kolapo on 8/1/2015.
  */
-public class EventRecycleViewAdapter {
+public class EventRecycleViewAdapter extends RecyclerView.Adapter<EventRecycleViewAdapter.ViewHolder>{
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView tvName;
+        public TextView tvDate;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.tvName = (TextView) itemView.findViewById(R.id.event_name);
+            this.tvDate = (TextView) itemView.findViewById(R.id.event_date);
+        }
+
+    }
+
+    private ArrayList<Event> events;
+    private Context context;
+
+    // Pass in the context and event list into the constructor
+    public EventRecycleViewAdapter(Context context, ArrayList<Event> events){
+        this.context = context;
+        this.events = events;
+    }
+
+    @Override
+    public EventRecycleViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflate the custom layout
+        View itemView = LayoutInflater.from(context).
+                inflate(R.layout.layout_event_row, parent, false);
+        // Return a new holder instance
+        return new EventRecycleViewAdapter.ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Event event = events.get(position);
+        // Set item views based on the data model
+        holder.tvName.setText(event.name);
+        holder.tvDate.setText(event.date);
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
 }
--- app/src/main/java/com/kolapo/ranti/MainActivity.java
+++ app/src/main/java/com/kolapo/ranti/MainActivity.java
@@ -3,16 +3,32 @@ package com.kolapo.ranti;
 import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvEvents = (RecyclerView) findViewById(R.id.rvEvents);
+        rvEvents.setHasFixedSize(true);
+
+        //Set layout manager to position the items
+        rvEvents.setLayoutManager(new LinearLayoutManager(this));
+
+        EventRecycleViewAdapter adapter = new EventRecycleViewAdapter(this, getEvents());
+        //Attach the adapter to the recyclerview to populate items
+        rvEvents.setAdapter(adapter);
+
     }
 
 
@@ -36,5 +52,14 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private ArrayList<Event> getEvents() {
+        ArrayList<Event> items = new ArrayList<Event>();
+        items.add(new Event("Biola's wedding anniversary", "30 March"));
+        items.add(new Event("Kolapo's birthday", "30 May"));
+        items.add(new Event("Christmas wishes to Tinu", "25 Dec"));
+        items.add(new Event("Easter message", "1 April"));
+        return items;
     }
 }
